5ff8eb2b49bcc670cc6ad1f77738b2035ea00a1b
pikagamed <51389229+pikagamed@users.noreply.github.com>
2019-06-05 11:41:59 +0800

    Class 'Dog' Declare and variable, method define

---
+++ UnityLearnCode/Assets/Scripts/Dog.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Dog 
+{
+    #region Variable Declare
+
+    public string name = "";
+    public float weight = 0F;
+    public string type = "米克斯";
+    public string sex = "";
+    public string color = "";
+    public int age = 0;
+
+    #endregion
+
+    #region Method Declare
+
+    public void Bark()
+    {
+        Debug.Log(name + "：汪汪");
+    }
+
+    #endregion
+}
