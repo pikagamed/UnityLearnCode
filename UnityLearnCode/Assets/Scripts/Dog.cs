9dd8175789c0cd2f20e4a3618b8c63e76c3cd732
pikagamed <51389229+pikagamed@users.noreply.github.com>
2019-06-05 13:11:29 +0800

    Update Class 'Dog' and 2 mothod declared

--- UnityLearnCode/Assets/Scripts/Dog.cs
+++ UnityLearnCode/Assets/Scripts/Dog.cs
@@ -15,9 +15,41 @@ public class Dog
 
     #region Method Declare
 
+    /// <summary>
+    /// 引導狗做出叫動作之方法
+    /// </summary>
     public void Bark()
     {
-        Debug.Log(name + "：汪汪");
+        Debug.Log(name + "：汪汪!!!");
+    }
+
+    /// <summary>
+    /// 轉換狗年齡之方法
+    /// </summary>
+    /// <returns>狗年齡×7</returns>
+    public int TurnToHumanAge()
+    {
+        return age * 7;
+    }
+
+    /// <summary>
+    /// 提示狗進食的訊息(速度)
+    /// </summary>
+    /// <param name="food">輸入食物</param>
+    /// <param name="speed">輸入速度、預設值為慢</param>
+    public void Eat(string food, string speed = "慢")
+    {
+        Debug.Log(name + "正在吃" + food + "，速度：" + speed);
+    }
+
+    /// <summary>
+    /// 提示狗進食的訊息(份量)
+    /// </summary>
+    /// <param name="food">輸入食物</param>
+    /// <param name="meal">輸入份量</param>
+    public void Eat(string food, int meal)
+    {
+        Debug.Log(name + "正在吃" + food + "，份量：" + meal);
     }
 
     #endregion
